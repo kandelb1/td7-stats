90b80970deb5801f9cb27c04f7c0b7be11365b2a
nickonometry <nickonometry@gmail.com>
2024-02-24 11:11:14 -0500

    feat(site): tweaked the page layout a bit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,25 +12,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={styles.body}>
-        <header className={styles.header}>
-          <div className={styles.topBar}>
-            <Link href={'/'}>
-			        <img src={'/td_top_logo.png'} width={126} height={96} alt={'ql logo'} className={styles.logo}/>
-            </Link>
-          </div>          
-          <Navbar entries={[
-            {name: 'Teams', link: '/teams'},
-            {name: 'Players', link: '/players'}
-            ]}
-          />
-        </header>
-        <div className={styles.topLevelDiv}>
-          {children}
+        <div className={styles.pageContainer}>
+          <header className={styles.header}>
+            <div className={styles.topBar}>
+              <Link href={'/'}>
+                <img src={'/td_top_logo.png'} width={126} height={96} alt={'ql logo'} className={styles.logo}/>
+              </Link>
+            </div>          
+            <Navbar entries={[
+              {name: 'Teams', link: '/teams'},
+              {name: 'Players', link: '/players'}
+              ]}
+            />
+          </header>
+          <div className={styles.topLevelDiv}>
+            {children}
+          </div>
+          <footer className={styles.footer}>
+            <Link href="https://twitch.tv/mrgrim" target="_blank"><img src={'/mrgrim.png'} width={82} height={80} alt={'mrgrim twitch'} /></Link>
+            <Link href="https://www.thunderdomequake.com"><img src={'/backbutton.png'} width={190} height={80} alt={'Return'} /></Link>
+          </footer>
         </div>
-        <footer className={styles.footer}>
-          <Link href="https://twitch.tv/mrgrim" target="_blank"><img src={'/mrgrim.png'} width={82} height={80} alt={'mrgrim twitch'} /></Link>
-          <Link href="https://www.thunderdomequake.com"><img src={'/backbutton.png'} width={190} height={80} alt={'Return'} /></Link>
-        </footer>
       </body>
     </html>
   )
